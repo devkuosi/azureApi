37ae957d38cb8d69fe5b7ca16259abef1179b9e9
kuosi <dev@kuosi.io>
2023-02-19 20:29:27 +0100

    add sessions

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, session, request, redirect, url_for
+from flask_session import Session  # https://pythonhosted.org/Flask-Session
+import app_config
+
 app = Flask(__name__)
+app.config.from_object(app_config)
+Session(app)
 
 @app.route("/home")
 def home():
