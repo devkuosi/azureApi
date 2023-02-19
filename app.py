52f32f1530be721718ba5bb8198aab54ae312bed
kuosi <dev@kuosi.io>
2023-02-19 09:46:20 +0100

    Change returned text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = flask.Flask(__name__)
 
 @app.route("/home")
 def home():
-    return "Flask API get endpoint running"
+    return "Flask API get endpoint running -----"
 
 if __name__ == "__main__":
     app.run()
