11e5a679b680ced01b9c1e697e2c242c4d67f6f8
kuosi <dev@kuosi.io>
2023-02-19 20:18:27 +0100

    return roles

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, render_template, session, request, redirect, url_for
+app = Flask(__name__)
 
 @app.route("/home")
 def home():
-    return "Flask API get endpoint running -----"
+    if not "roles" in session["user"]:
+        return "NO ROLE FOUND"
+    roles = ""
+    user_roles = session["user"]["roles"]
+    for user_role in user_roles:
+        roles = roles + "|" + user_role
+    return "----- " + roles
 
 if __name__ == "__main__":
     app.run()
