32ec78d5ff2bef358f215701f42ffd1b3fc1f29f
kuosi <dev@kuosi.io>
2023-02-21 05:23:39 +0100

    RBAC 18

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
 from flask_azure_oauth import FlaskAzureOauth
-import app_config
 
 app = Flask(__name__)
-app.config.from_object(app_config)
-app.config["DEBUG"] = True
 
 
 # Initialize the FlaskAzureOauth extension
